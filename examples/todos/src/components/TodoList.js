ade1156ca9c338b829953de689460e62e7e1a4ed
hijikiman <labomaster100@gmail.com>
2020-10-07 18:48:40 +0900

    [refact](todos) integrate Todo component into TodoList component and remove Todo

--- examples/todos/src/components/TodoList.js
+++ examples/todos/src/components/TodoList.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Todo from './Todo'
 
 const TodoList = ({ todos, toggleTodo }) => (
   <ul>
     {todos.map(todo =>
-      <Todo
-        key={todo.id}
-        {...todo}
+      <li
         onClick={() => toggleTodo(todo.id)}
-      />
+        style={{
+          textDecoration: todo.completed ? 'line-through' : 'none'
+        }}
+      >
+        {todo.text}
+      </li>
     )}
   </ul>
 )
